f2b3a47e4961255cc4a1f69f22ba2675e1387ac7
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:16:52 +0500

    Add files via upload

---
+++ Question 22/022.ts
@@ -0,0 +1,12 @@
+// 22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs
+// to produce an index error. Make sure you correct the error before closing the program.
+
+
+let myArry: (number|string)[] = ["ahad",22,"karachi",2436]
+
+console.log(myArry[0]);
+
+console.log(myArry[2]);
+
+//console.log(myArry[4]);
+console.log(myArry[3]);
